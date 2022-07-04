ece4647482dab22dd1029fa539e91f3eb24df47f
FranFerreyra98 <fran.ferreyra98@gmail.com>
2022-07-04 20:07:33 -0300

    1er Entrega Proyecto Final

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,42 @@
 //Solicitar la cantidad de productos que desea comprar
 let cantidadProductos = parseInt(prompt("Ingrese la cantidad de productos que desea comprar"));
 
+//Declaracion de clase y método constructor 
+
+class Producto {
+    constructor(nombre, precio, tipo) {
+        this.nombre  = nombre
+        this.precio  = precio
+        this.tipo = tipo
+        this.vendido = false;
+    }
+
+    extraerIva() {
+        this.iva = this.precio * .21;
+        alert("El iva de su producto es: "+ this.iva)
+    }
+
+}
+
+//Declaracion de array y nuevos objetos
+
+const productos = [];
+
+productos.push(new Producto("Aconcagua", 2200, "Gin"))
+productos.push(new Producto("Absolut", 3000, "Vodka"))
+productos.push(new Producto("Branca", 950, "Fernet"))
+
+const listaNombres = productos.map((producto) => producto.nombre);
+const listaPrecios = productos.map((producto) => producto.precio);
+
+function mostrarProductos(){
+    alert("Los productos disponibles son: " + listaNombres)
+}
+
+function mostrarPrecio(){
+    alert("Los precios de los productos son: " + listaPrecios)
+}
+
 //Declaracion de variables, funcion y ciclo for
 
 let carrito = 0
@@ -14,11 +50,9 @@ function solicitarProductos(){
 }
 
 for (let ciclos = 1; ciclos <= cantidadProductos; ciclos++){
-    solicitarProductos();
-
-    if(cantidadProductos == ciclos){
-        break
-    }
+    mostrarProductos()
+    mostrarPrecio()
+    solicitarProductos()
 }
 
 //Declaracion de variables, funcion y algoritmo condicional
@@ -26,21 +60,22 @@ for (let ciclos = 1; ciclos <= cantidadProductos; ciclos++){
 let codigoIngresado = false
 let finalizarCompra = false
 
-
-function aplicarDescuento(){
-    carrito = carrito * 0.90
-    alert("Descuento aplicado, la suma final de su carrito es: "+ carrito)
-}
-
 function preguntarCod(){
     let tieneCodigo = prompt("Tiene codigo de descuento? Si/No: ")
-    if (tieneCodigo === "Si"){
+    if (tieneCodigo == "Si"){
         solicitarDescuento()
         pagar()
     }
     else {
+        codigoIngresado = !codigoIngresado
+        finalizarCompra = !finalizarCompra
         pagar()
     }
+}
+
+function aplicarDescuento(){
+    carrito = carrito * 0.90
+    alert("Descuento aplicado, la suma final de su carrito es: "+ carrito)
 }
 
 function solicitarDescuento(){
@@ -61,3 +96,5 @@ if (codigoIngresado && finalizarCompra){
     alert ("Pasar a finalizar compra, el monto final del carrito es: "+ carrito)
     }
 }
+
+preguntarCod()
